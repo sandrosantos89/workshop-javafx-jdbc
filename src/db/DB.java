313a0bbc142fbc73a57220af30d9fdcf202d8a6c
Sandro Santos <sandro.ads.dev@gmail.com>
2022-02-10 22:17:50 -0300

    Adding database access

---
+++ src/db/DB.java
@@ -0,0 +1,75 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class DB {
+
+	// 1- Carregando o dados de login/ senha/ url do banco
+	private static Properties loadProperties() {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
+			Properties props = new Properties();
+			props.load(fs);
+			return props;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+	private static Connection conn = null;
+
+	// 2- Conectando ao banco de dados
+	public static Connection getConnection() {
+		if (conn == null) {
+			try {
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+
+		return conn;
+	}
+
+	// 3- Fechando o banco de dados
+	public static void closeConnection() {
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeStatement(Statement st) {
+		if (st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+	public static void closeResultSet(ResultSet rs) {
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+
+}
