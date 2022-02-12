efd8527ce1d3d9267257ac30c1f999502f863345
Sandro Santos <sandro.ads.dev@gmail.com>
2022-02-11 21:35:33 -0300

    saving a new department

--- src/model/services/DepartmentService.java
+++ src/model/services/DepartmentService.java
@@ -15,4 +15,12 @@ public class DepartmentService {
 
 	}
 
+	public void saveOurUpdate(Department obj) {
+		if (obj.getId() == null) {
+			dao.insert(obj);
+		} else {
+			dao.update(obj);
+		}
+	}
+
 }
